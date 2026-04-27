e5cc14a3bcbababd765db16cb369acd271541fea
RykhtaYaroslav <iupacmanshop@yandex.ru>
2026-04-28 04:03:02 +0700

    feat: Add ErrorResponse and ErrorHandler (add-controllers)
    Introduces a new ErrorResponse class and an ErrorHandler.
    The ErrorResponse class will be used to standardize error
    messages returned to the client. The ErrorHandler will
    catch specific exceptions like NotFoundException,
    MethodArgumentNotValidException, and
    DataIntegrityConflictException, log them, and return
    appropriate HTTP status codes and ErrorResponse objects.

---
+++ src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -0,0 +1,33 @@
+package ru.practicum.shareit.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class ErrorHandler {
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse NotFoundHandler(Exception e) {
+        log.error(e.getMessage());
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse ValidationHandler(Exception e) {
+        log.error(e.getMessage());
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(DataIntegrityConflictException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse ConflictHandler(Exception e) {
+        log.error(e.getMessage());
+        return new ErrorResponse(e.getMessage());
+    }
+}
