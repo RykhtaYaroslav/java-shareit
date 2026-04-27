db827f14902e51c856a42d731b8f3e30db72a9e7
RykhtaYaroslav <iupacmanshop@yandex.ru>
2026-04-28 03:43:55 +0700

    feat: Implement UserRepository and UserService (add-controllers)
    Adds UserInMemoryRepository, UserRepository interface, UserService
    interface and UserServiceImpl. This commit introduces the core
    components for user management, including in-memory storage,
    data access methods, and business logic for user operations.
    It also includes validation for email uniqueness and user
    existence checks, throwing appropriate exceptions.

--- src/main/java/ru/practicum/shareit/user/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/UserRepository.java
@@ -1,7 +1,18 @@
 package ru.practicum.shareit.user;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository {
     Optional<User> findById(Long userId);
+
+    Long create(User userCreateRequest);
+
+    User update(Long userId, User userUpdateRequest);
+
+    void deleteById(Long userId);
+
+    boolean isUniqueEmail(String email);
+
+    List<User> findAll();
 }
