70a40c70f462fd97cb04521aebeadaec9710b2de
RykhtaYaroslav <iupacmanshop@yandex.ru>
2026-06-16 23:49:21 +0700

    feat: Implement Booking entity and BookingStatus (add-bookings)
    Introduces a new BookingStatus enum to represent the state of a booking (WAITING, APPROVED, REJECTED, CANCELED). Transforms the Booking class into a JPA entity mapped to the "bookings" table, adding fields for ID, item ID, start and end dates, booker ID, and status. Additionally, utilizes Lombok annotations to generate constructors, getters, setters, and builder patterns to reduce boilerplate code.

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -1,7 +1,46 @@
 package ru.practicum.shareit.booking;
 
-/**
- * TODO Sprint add-bookings.
- */
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "bookings")
 public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "start_date", nullable = false)
+    private Long itemId;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDateTime startDate;
+
+    @Column(name = "end_date", nullable = false)
+    private LocalDateTime endDate;
+
+    @Column(name = "booker_id", nullable = false)
+    private Long bookerId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private BookingStatus status;
 }
