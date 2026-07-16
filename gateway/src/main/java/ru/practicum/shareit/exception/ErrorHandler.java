efbb79badccc5532db8ea9079f849849173ad96d
RykhtaYaroslav <iupacmanshop@yandex.ru>
2026-07-16 14:17:15 +0700

    feat(gateway): Add logging to exception handler
    This commit enhances the exception handling in the gateway module by adding
    logging for various exceptions.
    
    - `gateway/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java`:
      - Adds the `@Slf4j` annotation to enable logging.
      - Introduces a constant `DESCRIPTION` for error messages.
      - Adds logging statements to `handleValidation`, `handleConstraintViolation`,
        and `handleIllegalArgument` methods to log exceptions at the ERROR level.
      - Removes the generic `handleThrowable` exception handler, as specific
        handlers are now in place.

--- gateway/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ gateway/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.exception;
 
 import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,29 +11,28 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.Map;
 
 @RestControllerAdvice
+@Slf4j
 public class ErrorHandler {
+    private static final String DESCRIPTION = "Произошла ошибка при обработке входных данных: ";
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, String> handleValidation(final MethodArgumentNotValidException e) {
+        log.error(DESCRIPTION, e);
         return Map.of("error", String.format("Ошибка валидации: %s", e.getMessage()));
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, String> handleConstraintViolation(final ConstraintViolationException e) {
+        log.error(DESCRIPTION, e);
         return Map.of("error", String.format("Некорректный параметр запроса: %s", e.getMessage()));
     }
 
     @ExceptionHandler(IllegalBookingStateException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, String> handleIllegalArgument(final IllegalBookingStateException e) {
+        log.error(DESCRIPTION, e);
         return Map.of("error", e.getMessage());
-    }
-
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public Map<String, String> handleThrowable(final Throwable e) {
-        return Map.of("error", String.format("Произошла непредвиденная ошибка: %s", e.getMessage()));
     }
 }
